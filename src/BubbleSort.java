9b3c8383df6b95a00bc19581f3b6d34d56f84b73
Lutsak Yevhen <yev.lutsak@gmail.com>
2018-07-05 15:54:18 +0300

    Bubble sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,39 @@
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] array = { 4, 2, 9, 6, 23, 12, 34, 0, 1 };
+
+        //Print unsorted array.
+        System.out.print("Unsorted array :");
+        for (int anInput : array) {
+            System.out.print(anInput + " ");
+        }
+        System.out.println();
+
+        //Sort array.
+        int k;
+
+        for (int i = 0; i <= array.length; i++) {
+
+            for (int j = 0; j < array.length - 1; j++) {
+
+                k = j + 1;
+
+                if (array[j] > array[k]) {
+
+                    int temp;
+                    temp = array[j];
+                    array[j] = array[k];
+                    array[k] = temp;
+
+                }
+            }
+        }
+
+        //Print sorted array.
+        System.out.print("Sorted array   :");
+        for (int anInput : array) {
+            System.out.print(anInput + " ");
+        }
+    }
+}
