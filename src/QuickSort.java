89c823f3c010e619f25150d7a013b4cbfdae035d
Lutsak Yevhen <yev.lutsak@gmail.com>
2018-07-05 18:58:06 +0300

    QuickSort

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,4 +1,54 @@
-package PACKAGE_NAME;
-
 public class QuickSort {
+
+    private static int[] array = {14, 26, 12};
+
+    private static void quickSort(int lowerIndex, int higherIndex) {
+
+        int lower = lowerIndex;
+        int higher = higherIndex;
+        int pivot = array[lowerIndex + (higherIndex - lowerIndex) / 2];
+
+        while (lower <= higher) {
+
+            while (array[lower] < pivot) {
+                lower++;
+            }
+
+            while (array[higher] > pivot) {
+                higher--;
+            }
+
+            if (lower <= higher) {
+                int temp = array[lower];
+                array[lower] = array[higher];
+                array[higher] = temp;
+                lower++;
+                higher--;
+            }
+        }
+        // call quickSort() method recursively
+        if (lowerIndex < higher)
+            quickSort(lowerIndex, higher);
+        if (lower < higherIndex)
+            quickSort(lower, higherIndex);
+    }
+
+    public static void main(String[] args){
+
+        //Print unsorted array.
+        System.out.print("Unsorted array :");
+        for (int anInput : array) {
+            System.out.print(anInput + " ");
+        }
+        System.out.println();
+
+        quickSort(0, array.length - 1);
+
+        //Print sorted array.
+        //Print sorted array.
+        System.out.print("Sorted array   :");
+        for (int anInput : array) {
+            System.out.print(anInput + " ");
+        }
+    }
 }
